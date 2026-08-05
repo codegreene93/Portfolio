35285c851c6d05d7cce0adddd282a0a9e3d493ca
Amy Greene <amy.greene@viperinnovations.com>
2026-08-05 21:24:40 +0100

    add portflio data, generate images for them

---
+++ src/data/portfolioData.ts
@@ -0,0 +1,36 @@
+
+
+export const portfolioData = [
+  {
+    id: 1,
+    title: 'Dog of the Day',
+    description: 'This is my portfolio website.',
+    imgUrl: '../assets/images/dog.jpg',
+    github: 'https://github.com/Sahil-Kumar-M/portfolio',
+    url: 'https://github.com/Sahil-Kumar-M/portfolio',
+  },
+    {
+        id: 2,
+        title: 'Flashcards',
+        description: 'This is my portfolio website.',
+        imgUrl: '../assets/images/FriendsInKitchen.jpg',
+        github: 'https://github.com/Sahil-Kumar-M/portfolio',
+        url: 'https://github.com/Sahil-Kumar-M/portfolio',
+    },
+    {
+        id: 3,
+        title: 'Hotel Site',
+        description: 'This is my portfolio website.',
+        imgUrl: '../assets/images/Santorini.jpg',
+        github: 'https://github.com/Sahil-Kumar-M/portfolio',
+        url: 'https://github.com/Sahil-Kumar-M/portfolio',
+    },
+    {
+        id: 4,
+        title: 'Scratch Map',
+        description: 'This is my portfolio website.',
+        imgUrl:  '../assets/images/scratchMap.jpg',
+        github: 'https://github.com/Sahil-Kumar-M/portfolio',
+        url: 'https://github.com/Sahil-Kumar-M/portfolio',
+    },
+];
